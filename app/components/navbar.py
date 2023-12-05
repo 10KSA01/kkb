46c8539bca85007171700d8e3a0f5aa7cc2b7d72
BenSnellgrove <ben.e.snellgrove@gmail.com>
2023-12-05 12:29:22 +0400

    Fixed broken link in navbar to quizStart.py

--- app/components/navbar.py
+++ app/components/navbar.py
@@ -7,7 +7,7 @@ def create_navbar():
             dbc.NavItem(dbc.NavLink("Main Page", href="/")),
             dbc.NavItem(dbc.NavLink("Page 1", href="/page1")),
             dbc.NavItem(dbc.NavLink("Page 2", href="/page2")),
-            dbc.NavItem(dbc.NavLink("quiz start broken", href="/quiz_start")),
+            dbc.NavItem(dbc.NavLink("Quiz start demo", href="/quizstart")),
         ],
         brand="KBB",
         brand_href="/",
