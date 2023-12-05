da04afbbbffd10527119de043327bf1cbeb30cc5
Kumar Acharjee <kumaracharjee@outlook.com>
2023-12-05 15:45:32 +0000

    shop

--- app/pages/shop.py
+++ app/pages/shop.py
@@ -7,11 +7,12 @@ register_page(__name__)
 
 layout = dbc.CardBody(
     [
+        html.Br(),
         dbc.Row([
             dbc.Col(
                 [
                     html.H2("Welcome to the Shop"),
-                    html.P("Explore our collection of items and make your purchase."),
+                    html.P("Explore our collection of accessories and make your purchase."),
                 ],
                 width=12, className="text-center"
             ),
@@ -19,12 +20,26 @@ layout = dbc.CardBody(
         html.Br(),
         dbc.Row(
             [
-                dbc.Col([example_shop_item("item 1 $5")], width=3),
-                dbc.Col([example_shop_item("item 2 $3")], width=3),
-                dbc.Col([example_shop_item("item 3 $10")], width=3),
-                dbc.Col([example_shop_item("item 4 $100")], width=3),
-            ],
-            align="center",
+                dbc.Col(
+                    [
+                        dbc.Row(
+                            [
+                                dbc.Col([example_shop_item(f"item {i} ${i * 2}")], width=3)
+                                for i in range(1, 9)  # Adjust the range based on the number of items
+                            ],
+                            className="overflow-auto",  # Enable horizontal scrolling
+                            style={"flex-wrap": "nowrap"},  # Prevent items from wrapping to the next line
+                        ),
+                    ],
+                    width=11,
+                    align="center",
+                    style={
+                        "display": "flex", 
+                        "justify-content": "center", 
+                        "align-items": "center", 
+                    }
+                ),
+            ]
         ),
         html.Br(),
     ]
