b85fa34271fe26d96659e50715d7739e291e3aea
Kaleem Peeroo <mail@kaleempeeroo.com>
2023-12-06 06:25:33 +0000

    Redesigned quiz results page

--- app/pages/quizResults.py
+++ app/pages/quizResults.py
@@ -2,9 +2,9 @@ import dash
 from dash import Dash, html, dcc, register_page
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
+from components.Items import example_daily_item
 import math
 import platform
-
 
 if platform.system() == 'Windows':
     score_file_path = "app/temp/cur_quiz_score.tmp"
@@ -13,11 +13,7 @@ else:
     score_file_path = "temp/cur_quiz_score.tmp"
     answer_file_path = "temp/cur_quiz_answered.tmp"
 
-
-
 register_page(__name__)
-
-
 
 stars = 3
 stars_unicode = "".join(["★"] * stars + ["☆"] * (3-stars))
@@ -27,104 +23,296 @@ with open(score_file_path, "r") as f_r:
 with open(answer_file_path, "r") as f_r:
     answered = int(f_r.read())
 
+def create_scoreboard_list(type):
+    if type == "friends":
+        friend_names = ["bob", "dave", "steve", "mary"]
+    else:
+        friend_names = ["", "", "", ""]
 
-# Use the Dash app with a Bootstrap theme
+    return dbc.ListGroup(
+        style={},
+        children=[
+            create_scoreboard_list_item(
+                1, 
+                friend_names[0],
+                80,
+                "10:34"
+            ),
+            create_scoreboard_list_item(
+                2, 
+                friend_names[1],
+                80,
+                "12:34"
+            ),
+            create_scoreboard_list_item(
+                3, 
+                friend_names[2],
+                40,
+                "6:34"
+            ),
+            create_scoreboard_list_item(
+                4, 
+                friend_names[3],
+                8,
+                "1:34"
+            ),
+        ]
+    )
 
-layout = dbc.Container([
-    # First Row
-    dbc.Row([
-        dbc.Col(
-            dbc.Card([
-                html.H3("Quiz title"),
-            ]),
-            width=6  # This column takes half of the row
+def create_scoreboard_list_item(ranking, friend_name, score_percentage, time):
+    return dbc.ListGroupItem(
+        style={
+            "display": "flex",
+            "justify-content": "space-between",
+        },
+        children=[
+            html.Span(str(ranking)),
+            html.Span(friend_name),
+            dbc.Progress(label=str(score_percentage) + "%", value=score_percentage, style={"width": "50%"}),
+            html.Span(time),
+        ]
+    )
+
+layout = html.Div(
+    style={
+        "padding": "2vh 2vw"
+    },
+    children=[
+        
+        # Top row with quiz name and next button
+        html.Div(
+            style={
+                "display": "flex",
+                "justify-content": "space-between",
+            },
+            children=[
+                html.H3("Quiz Name"),
+                dbc.Button(
+                    style={"textAlign": "left"}, 
+                    children=[
+                        html.Span("Next Quiz", style={"fontSize": "1.2em"}),
+                        html.Br(),
+                        html.Span("Algebra"),
+                    ],
+                    color="primary", 
+                    href="/quizstart"
+                ),
+            ]
         ),
-        dbc.Col(
-            dbc.Card([
-                html.H3(f"Difficulty: {stars_unicode}"),
-            ]),
-            width=4
-        ),
 
-        dbc.Col(
-            dbc.Card([
-                dbc.Button([
-                    html.H3("Next Quiz")
-                ], color="primary", style={"width": "100%"}, href="/quizstart"),
-            ]),
-            width=2 # last 2/12ths of a row
-        ),
-    ],
-        className="m-2",
-    ),
-
-    # level up bar row
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                html.H2("Level 9"),
+        # Progress bar row
+        html.Div(
+            style={
+                "display": "flex",
+                "justify-content": "space-around",
+                "padding": "10vh 0"
+            },
+            children=[
+                html.H2("9"),
                 dbc.Progress(
                     [
-                        dbc.Progress(value=0, color="warning", bar=True, id="xp-bar-base"),
-                        dbc.Progress(value=10, color="success", bar=True, id="xp-bar-growth"),
-                    ]
+                        dbc.Progress(value=0, color="primary", bar=True, id="xp-bar-base"),
+                        dbc.Progress(value=10, color="success", bar=True, id="xp-bar-growth", label="+27 XP", striped=True, animated=True),
+                    ],
+                    style={
+                        "width": "90%",
+                        "height": "5vh"
+                    }
                 ),
                 dcc.Interval(
                     id="xp-bar-timer",
                     interval=100,  # Update every millisecond
                     n_intervals=0,
                     disabled=False
+                ),
+                html.H2("10")
+            ]
+        ),
+
+        # Reward row
+        html.Div(
+            style={
+                "padding": "5vh 0 ",
+            },
+            children=[
+                dbc.Row(
+                    style={
+                        "flex-wrap": "nowrap",
+                        "display": "flex",
+                        "justify-content": "center"
+                    },
+                    children=[
+                        dbc.Col([example_daily_item("Title", "Algebra Admiral", check=False)], width={"size": 2}),
+                        dbc.Col([example_daily_item("Points", "+15 points", check=False)], width={"size": 2}),
+                    ],
+                    className="overflow-auto",
+            ),
+            ]
+        ),
+
+        # Score breakdown row
+        html.Div(
+            style={},
+            children=[
+                html.H3("Score: 3/5 (60%)"),
+                html.H3("Quiz breakdown"),
+                html.P("Maybe by question")
+            ]
+        ),
+
+        # Scoreboard
+        html.Div(
+            style={},
+            children=[
+                dbc.Card(
+                    style={},
+                    children=[
+                        dbc.Tabs(
+                            style={},
+                            children=[
+                                dbc.Tab(
+                                    label="Your Scores", 
+                                    tab_id="tab-1",
+                                    children=[
+                                        html.Div(
+                                            style={},
+                                            children=[
+                                                create_scoreboard_list(None)
+                                            ]
+                                        ),
+                                    ]
+                                ),
+                                dbc.Tab(
+                                    label="Friends", 
+                                    tab_id="tab-2",
+                                    children=[
+                                        html.Div(
+                                            style={},
+                                            children=[
+                                                create_scoreboard_list("friends")
+                                            ]
+                                        ),
+                                    ]
+                                ),
+                                dbc.Tab(
+                                    label="Global", 
+                                    tab_id="tab-3",
+                                    children=[
+                                        html.Div(
+                                            style={},
+                                            children=[
+                                                create_scoreboard_list("friends")
+                                            ]
+                                        ),
+                                    ]
+                                ),
+                            ],
+                        )
+                    ]
                 )
-            ])
+            ]
+        )
 
-        ], width=9),
 
-        dbc.Col([
-            dbc.Card([
-                html.H2("+27 XP")
-            ])
-        ], width=3),
-    ],
-        className="m-2",
-    ),
 
-    dbc.Row([
-        dbc.Col([
+    ]
+)
 
-            dbc.Row([
-                dbc.Card([
-                    html.H3(f"Score: {correct}/{answered} ({((correct * 100) // answered) if answered != 0 else '--'}%)",
-                            id="quizscore")
-                ]),
-            ]),
+# layout = dbc.Container([
+#     # First Row
+#     dbc.Row([
+#         dbc.Col(
+#             dbc.Card([
+#                 html.H3("Quiz title"),
+#             ]),
+#             width=6  # This column takes half of the row
+#         ),
+#         dbc.Col(
+#             dbc.Card([
+#                 html.H3(f"Difficulty: {stars_unicode}"),
+#             ]),
+#             width=4
+#         ),
 
-            dbc.Row([
+#         dbc.Col(
+#             dbc.Card([
+#                 dbc.Button([
+#                     html.H3("Next Quiz")
+#                 ], color="primary", style={"width": "100%"}, href="/quizstart"),
+#             ]),
+#             width=2 # last 2/12ths of a row
+#         ),
+#     ],
+#         className="m-2",
+#     ),
 
-                dbc.Card([
-                    html.H3("Quiz breakdown"),
-                    html.P("Maybe by question")
-                ]),
-            ]),
+#     # level up bar row
+#     dbc.Row([
+#         dbc.Col([
+#             dbc.Card([
+#                 html.H2("Level 9"),
+#                 dbc.Progress(
+#                     [
+#                         dbc.Progress(value=0, color="warning", bar=True, id="xp-bar-base"),
+#                         dbc.Progress(value=10, color="success", bar=True, id="xp-bar-growth"),
+#                     ]
+#                 ),
+#                 dcc.Interval(
+#                     id="xp-bar-timer",
+#                     interval=100,  # Update every millisecond
+#                     n_intervals=0,
+#                     disabled=False
+#                 )
+#             ])
 
-        ],
-            width=6,
-        ),
+#         ], width=9),
 
-        # Second Column in the second row
-        dbc.Col([
-            dbc.Row([
+#         dbc.Col([
+#             dbc.Card([
+#                 html.H2("+27 XP")
+#             ])
+#         ], width=3),
+#     ],
+#         className="m-2",
+#     ),
 
-                dbc.Card([
-                    html.H3("Friend scoreboard"),
-                ]),
-            ]),
-        ],
-            width=6
-        ),
-    ],
-        className="m-3"
-    ),
-])
+#     dbc.Row([
+#         dbc.Col([
+
+#             dbc.Row([
+#                 dbc.Card([
+#                     html.H3(f"Score: {correct}/{answered} ({((correct * 100) // answered) if answered != 0 else '--'}%)",
+#                             id="quizscore")
+#                 ]),
+#             ]),
+
+#             dbc.Row([
+
+#                 dbc.Card([
+#                     html.H3("Quiz breakdown"),
+#                     html.P("Maybe by question")
+#                 ]),
+#             ]),
+
+#         ],
+#             width=6,
+#         ),
+
+#         # Second Column in the second row
+#         dbc.Col([
+#             dbc.Row([
+
+#                 dbc.Card([
+#                     html.H3("Friend scoreboard"),
+#                 ]),
+#             ]),
+#         ],
+#             width=6
+#         ),
+#     ],
+#         className="m-3"
+#     ),
+# ])
 
 
 @dash.callback(
